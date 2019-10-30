fe14c28bd3586fe993c1c6b8c8277fcfe48a1d24
Oscar Garcia Masia <ogarciamasia@gmail.com>
2019-10-30 19:59:59 +0100

    add built files in apache built

---
+++ apache built/dependencies/index.js
@@ -0,0 +1,4 @@
+import React from "https://unpkg.com/react@16/umd/react.development.js";
+import "./styles.css";
+const MyComponent = () => <h1>Hello from My Component</h1>;
+export default MyComponent;
