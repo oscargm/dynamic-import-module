41fa79361536030026b950f7d21a1bb9e08a3570
Oscar Garcia Masia <ogarciamasia@gmail.com>
2019-10-31 10:06:24 +0100

    not the bes solution

--- apache built/index.js
+++
@@ -1,8 +0,0 @@
-import MyComponent from "./dependencies";
-import React from "https://unpkg.com/react@16/umd/react.development.js";
-import { render } from "https://unpkg.com/react-dom@16/umd/react-dom.development.js";
-
-export default function init(elementId) {
-  const App = () => <MyComponent />;
-  render(<App />, document.getElementById(elementId));
-}
--- apache-built/dependencies/index.js
+++ apache-built/dependencies/index.js
@@ -1,4 +1,4 @@
 import React from "https://unpkg.com/react@16/umd/react.development.js";
 import "./styles.css";
-const MyComponent = () => <h1>Hello from My Component</h1>;
+const MyComponent = () => <h2>Hello from My importedComponent</h2>;
 export default MyComponent;
